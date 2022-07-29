b3c0f5503becdbf920d7c7891123b8af8d662167
Talley Lambert <talley.lambert@gmail.com>
2022-07-29 19:43:54 -0400

    use send not json

--- pages/api/manifest/[slug].js
+++ pages/api/manifest/[slug].js
@@ -8,5 +8,5 @@ export default async function handler(req, res) {
     jsonDirectory + `/${slug}.json`,
     "utf8"
   );
-  res.status(200).json(fileContents);
+  res.status(200).send(fileContents);
 }
--- pages/api/readers.js
+++ pages/api/readers.js
@@ -4,5 +4,5 @@ import { promises as fs } from 'fs';
 export default async function handler(req, res) {
   const jsonDirectory = path.join(process.cwd(), 'public');
   const fileContents = await fs.readFile(jsonDirectory + '/readers.json', 'utf8');
-  res.status(200).json(fileContents);
+  res.status(200).send(fileContents);
 }
