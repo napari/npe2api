566066f234ebae7456f14f5a6e989308d8fd850b
Talley Lambert <talley.lambert@gmail.com>
2022-08-03 22:32:05 -0400

    v2

--- pages/api/summary.js
+++ pages/api/summary.js
@@ -1,10 +1,11 @@
 import path from 'path';
 import { promises as fs } from 'fs';
-import supabase from '../../lib/supabase/private';
+import logHit from '../../lib/supabase/private';
 
 export default async function handler(req, res) {
-  await supabase.rpc('increment_views', { page_slug: req.url });
+  console.log(JSON.stringify(req.headers));
   const jsonDirectory = path.join(process.cwd(), 'public');
   const fileContents = await fs.readFile(jsonDirectory + '/summary.json', 'utf8');
+  logHit(req);
   res.status(200).send(JSON.stringify(JSON.parse(fileContents)));  // remove whitespace
 }
