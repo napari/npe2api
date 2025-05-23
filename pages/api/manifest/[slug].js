bfb162e5571c6c8805806bff48d63868f440c5e2
Draga Doncila <ddon0001@student.monash.edu>
2025-05-23 18:32:39 +1000

    Rename manifest files using normalized name and reroute api endpoint

--- pages/api/manifest/[slug].js
+++ pages/api/manifest/[slug].js
@@ -3,10 +3,15 @@ import { promises as fs } from "fs";
 
 export default async function handler(req, res) {
   const { slug } = req.query;
+  const normalizedSlug = normalizeName(slug);
   const jsonDirectory = path.join(process.cwd(), "public", "manifest");
   const fileContents = await fs.readFile(
-    jsonDirectory + `/${slug}.json`,
+    jsonDirectory + `/${normalizedSlug}.json`,
     "utf8"
   );
   res.status(200).send(JSON.stringify(JSON.parse(fileContents)));
+}
+
+function normalizeName(name) {
+  return name.replace(/[-_.]+/g, "-").toLowerCase();
 }
